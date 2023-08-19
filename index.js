fe0be6b8c0916743adcfad6d3d2c617814d61f52
Yashraj Jain <yashrajjain726@gmail.com>
2023-08-19 22:21:59 +0530

    refractoring done

--- index.js
+++ index.js
@@ -98,12 +98,13 @@ const deleteTour = (req, res) => {
     }
   );
 };
-app.get('/api/v1/tours', getAllTours);
-app.get('/api/v1/tours/:id', getTour);
-app.post('/api/v1/tours', addTour);
-app.patch('/api/v1/tours/:id', updateTour);
-app.delete('/api/v1/tours/:id', deleteTour);
 
+app.route('/api/v1/tours').get(getAllTours).post(addTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}...`);
 });
