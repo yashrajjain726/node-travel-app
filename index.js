736cd468f30ff30d36f0aa89aaec930dbb75a1d0
Yashraj Jain <yashrajjain726@gmail.com>
2023-08-19 17:59:36 +0530

    added sample data and a get api

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const PORT = 3000 || process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the API service' });
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/tours-simple.json`)
+);
 
+app.get('/api/v1/tours', (req, res) => {
+  res.json({
+    status: 200,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}...`);
 });
