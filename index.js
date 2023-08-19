efe5994c0ddfafe914b28dcbe5d33289ad4f0b90
Yashraj Jain <yashrajjain726@gmail.com>
2023-08-19 18:54:20 +0530

    added post api and written data to the file

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const fs = require('fs');
 const PORT = 3000 || process.env.PORT;
 
+app.use(express.json());
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/data/tours-simple.json`)
 );
@@ -16,6 +18,26 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+
+app.post('/api/v1/tours', (req, res) => {
+  const { name, duration, difficulty, price, description } = req.body;
+  const id = tours[tours.length - 1].id + 1;
+  const newTour = { id, name, duration, difficulty, price, description };
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (callback) => {
+      res.json({
+        message: 'Your data has been added successfully.',
+        status: 201,
+        data: { tour: newTour },
+      });
+    }
+  );
+});
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}...`);
 });
