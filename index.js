5269f00a05e807b8fbe5ecd33e3752191181fcea
Yashraj Jain <yashrajjain726@gmail.com>
2023-08-22 22:50:51 +0530

    env variable revision

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 // ROUTES
@@ -6,6 +7,9 @@ const toursRouter = require('./routes/tourRoutes');
 const usersRouter = require('./routes/userRoutes');
 
 // MIDDLEWARE
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev')); // MORGAN IS USED AS A LOGGER FOR  DEVELOPMENT MODE
+}
 app.use(express.json());
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
