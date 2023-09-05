4c4512d0e9e250fee7ede681dc439836c6f69438
Yashraj Jain <yashrajjain726@gmail.com>
2023-09-05 15:39:42 +0530

    added mongodb config

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
 const app = require('./index');
 
 const PORT = process.env.PORT || 3000;
+const DB = process.env.MONGODB_URL.replace(
+  '<PASSWORD>',
+  process.env.MONGODB_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log(`Connected to Database`);
+  });
 
 // SERVER CONFIG
 app.listen(PORT, () => {
