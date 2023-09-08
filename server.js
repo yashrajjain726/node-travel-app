42fa64d3a6aacf2c217ed8361dd4b99898831708
Yashraj Jain <yashrajjain726@gmail.com>
2023-09-08 09:15:28 +0530

    creating mongoose model and schema

--- server.js
+++ server.js
@@ -19,6 +19,24 @@ mongoose
     console.log(`Connected to Database`);
   });
 
+//  MODELS
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour name must be provided'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour price must be provided'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
 // SERVER CONFIG
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}...`);
