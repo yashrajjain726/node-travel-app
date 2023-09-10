e2ee8c89db6ec6da8d2c24896e1111b0b0953613
Yashraj Jain <yashrajjain726@gmail.com>
2023-09-10 10:22:18 +0530

    new schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,14 +6,51 @@ const tourSchema = mongoose.Schema({
     required: [true, 'A tour name must be provided'],
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour duration must be provided'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour group size must be provided'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour difficulty must be provided'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour price must be provided'],
   },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour summary must be provided'],
+  },
+  description: { type: String, trim: true },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour image must be provided'],
+  },
+  images: {
+    type: [String],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: { type: [Date] },
 });
 const Tour = mongoose.model('Tour', tourSchema);
 
